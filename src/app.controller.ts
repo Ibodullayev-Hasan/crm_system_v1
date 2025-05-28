3a60c1b5ce180740dd3dd54aefe5fe468328a1ca
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-05-28 22:09:44 +0500

    file created

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+
+import { Get, Controller, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller()
+export class AppController {
+	@Get()
+	getHello(@Res() res: Response) {
+		res.render('hello', { name: 'CRM' });
+	}
+
+}
